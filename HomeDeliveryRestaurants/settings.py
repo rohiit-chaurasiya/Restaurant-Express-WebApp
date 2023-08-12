b731d8a5c412d8fd4e6011532f8e90f7ecddb1c4
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-12 15:44:10 +0530

    Update settings.py

--- HomeDeliveryRestaurants/settings.py
+++ HomeDeliveryRestaurants/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from decouple import config
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
