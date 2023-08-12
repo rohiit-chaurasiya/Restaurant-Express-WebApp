57b1dc40fe19cc9e4e3b851f0616681198f75ed1
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-12 15:22:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 python3.9 -m pip install django
+python3.9 -m pip install PyMySQL
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
