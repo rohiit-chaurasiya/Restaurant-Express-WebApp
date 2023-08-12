a900935ab4e8dbe2c66ae6129f14309cf36b953b
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-12 15:03:30 +0530

    Update settings.py

--- HomeDeliveryRestaurants/settings.py
+++ HomeDeliveryRestaurants/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-*g=u%*=f35#hdj**31-yzw4giv*9-rf)5-(z+kihn5afoyjln5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [',vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 # "127.0.0.1", "192.168.1.6","localhost"
 
 # Application definition
