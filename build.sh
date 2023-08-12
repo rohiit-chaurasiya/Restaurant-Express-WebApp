4c8071b38db4a0b3a14b4451847ab7687d107d5d
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-12 15:49:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ python3.9 -m pip install django
 python3.9 -m pip install PyMySQL
 python3.9 -m pip install dj-database-url
 python3.9 -m pip install django-active-link
+python3.9 -m pip install razorpay
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
